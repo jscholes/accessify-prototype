054ad44575d20bdc0d4c79c75c1bc81d5e4e50e7
James Scholes <james@jamesscholes.com>
2017-02-18 16:45:04 +0000

    Remove checking of send_message result as Windows doesn't seem to care if we send to a non-existent HWND

--- accessify/spotify.py
+++ accessify/spotify.py
@@ -47,17 +47,8 @@ def send_command(command_id):
     hwnd = find_window(SPOTIFY_WINDOW_CLASS, None)
     if hwnd == 0:
         raise SpotifyWindowNotFoundError
-    result = send_message(hwnd, WM_COMMAND, command_id, 0)
-    if result != 0:
-        raise CommandError(result=result)
+    send_message(hwnd, WM_COMMAND, command_id, 0)
 
 
 class WindowNotFoundError(Exception):
     """Raised when the HWND of the Spotify main window cannot be found."""
-
-
-class CommandError(Exception):
-    """Raised when sending a message to the Spotify window does not complete successfully."""
-    def __init__(self, *args, **kwargs):
-        self.result = kwargs.pop(result)
-        super().__init__(*args, **kwargs)
