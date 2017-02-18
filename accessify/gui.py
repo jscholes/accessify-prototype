719cc275329200b09eb943294065931422e8c1b9
James Scholes <james@jamesscholes.com>
2017-02-18 16:51:43 +0000

    Extract GUI labels out to module-level constants

--- accessify/gui.py
+++ accessify/gui.py
@@ -6,6 +6,15 @@ import spotify
 
 
 WINDOW_TITLE = 'Accessify'
+MENU_PLAYBACK = '&Playback'
+LABEL_PLAY_PAUSE = 'P&lay/Pause'
+LABEL_PREVIOUS = 'P&revious'
+LABEL_NEXT = '&Next'
+LABEL_REWIND = 'Re&wind'
+LABEL_FAST_FORWARD = '&Fast Forward'
+LABEL_INCREASE_VOLUME = '&Increase Volume'
+LABEL_DECREASE_VOLUME = '&Decrease Volume'
+
 
 PlaybackCommand = namedtuple('PlaybackCommand', ['label', 'func', 'hotkey', 'show_as_button'])
 
@@ -15,13 +24,13 @@ class MainWindow(wx.Frame):
         super().__init__(parent=None, title=WINDOW_TITLE, *args, **kwargs)
         self.panel = wx.Panel(self)
         self.commands = {
-            wx.NewId(): PlaybackCommand('P&lay/Pause', spotify.play_pause, 'Space', True),
-            wx.NewId(): PlaybackCommand('P&revious', spotify.previous_track, 'Ctrl+Left', True),
-            wx.NewId(): PlaybackCommand('&Next', spotify.next_track, 'Ctrl+Right', True),
-            wx.NewId(): PlaybackCommand('Re&wind', spotify.seek_backwards, 'Shift+Left', True),
-            wx.NewId(): PlaybackCommand('&Fast Forward', spotify.seek_forwards, 'Shift+Right', True),
-            wx.NewId(): PlaybackCommand('&Increase Volume', spotify.increase_volume, 'Ctrl+Up', False),
-            wx.NewId(): PlaybackCommand('&Decrease Volume', spotify.decrease_volume, 'Ctrl+Down', False),
+            wx.NewId(): PlaybackCommand(LABEL_PLAY_PAUSE, spotify.play_pause, 'Space', True),
+            wx.NewId(): PlaybackCommand(LABEL_PREVIOUS, spotify.previous_track, 'Ctrl+Left', True),
+            wx.NewId(): PlaybackCommand(LABEL_NEXT, spotify.next_track, 'Ctrl+Right', True),
+            wx.NewId(): PlaybackCommand(LABEL_REWIND, spotify.seek_backwards, 'Shift+Left', True),
+            wx.NewId(): PlaybackCommand(LABEL_FAST_FORWARD, spotify.seek_forwards, 'Shift+Right', True),
+            wx.NewId(): PlaybackCommand(LABEL_INCREASE_VOLUME, spotify.increase_volume, 'Ctrl+Up', False),
+            wx.NewId(): PlaybackCommand(LABEL_DECREASE_VOLUME, spotify.decrease_volume, 'Ctrl+Down', False),
         }
         self.setup_commands(self.commands)
 
@@ -34,7 +43,7 @@ class MainWindow(wx.Frame):
             playback_menu.Append(id, '{0}\t{1}'.format(command.label, command.hotkey))
             self.Bind(wx.EVT_MENU, self.onPlaybackCommand)
         menu_bar = wx.MenuBar()
-        menu_bar.Append(playback_menu, '&Playback')
+        menu_bar.Append(playback_menu, MENU_PLAYBACK)
         self.SetMenuBar(menu_bar)
 
     def onPlaybackCommand(self, event):
