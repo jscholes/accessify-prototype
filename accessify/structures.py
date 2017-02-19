22393119db3a03c641a723b0cad19923685b48f1
James Scholes <james@jamesscholes.com>
2017-02-19 22:50:22 +0000

    Add artist field to Album structure

--- accessify/structures.py
+++ accessify/structures.py
@@ -3,6 +3,6 @@ from collections import namedtuple
 
 Track = namedtuple('Track', ['artist', 'album', 'name', 'length', 'type', 'uri'])
 Artist = namedtuple('Artist', ['name', 'uri'])
-Album = namedtuple('Album', ['name', 'uri'])
+Album = namedtuple('Album', ['artist', 'name', 'uri'])
 PlaybackStatus = namedtuple('PlaybackStatus', ['playing', 'position', 'volume', 'repeat', 'shuffle'])
 SpotifyStatus = namedtuple('SpotifyStatus', ['client_version', 'running'])
