e95277213ed66af75daaaf85dc4e4fad18a79ef0
James Scholes <james@jamesscholes.com>
2017-03-24 14:53:49 +0000

    Load some sample JSON responses downloaded from the Spotify web API instead of hard-coded sample results

--- accessify/search.py
+++ accessify/search.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import logging
 
+from functional import seq
+
 from . import structures
 
 
@@ -14,7 +16,22 @@ class SearchController:
     def perform_new_search(self, query, search_type):
         search_type = search_type.value
         logger.debug('Searching for {0} (search type: {1})'.format(query, search_type))
-        return self.api_client.search(query, search_type)
+        results = self.api_client.search(query, search_type)
+        if results:
+            return (seq(results['tracks']['items'])
+                .map(deserialize_item)
+            )
+        else:
+            return []
+
+
+def deserialize_item(item):
+    artist_node = item['artists'][0]
+    artist = structures.Artist(name=artist_node['name'], uri=artist_node['uri'])
+    album_node = item['album']
+    album = structures.Album(artist=artist, name=album_node['name'], uri=album_node['uri'])
+    track = structures.Track(artist=artist, name=item['name'], uri=item['uri'], album=album, length=item['duration_ms'] / 1000)
+    return track
 
 
 class SearchType(Enum):
--- accessify/spotify/webapi/client.py
+++ accessify/spotify/webapi/client.py
@@ -1,34 +1,18 @@
-from collections import defaultdict
 import os.path
 
 import ujson as json
 
-from accessify import structures
-
 
 class WebAPIClient:
     def search(self, query, search_type):
-        return sample_results[search_type]
-
-
-sample_results = defaultdict(list)
-
-sample_tracks = [
-    structures.Track(structures.Artist('Sharon Shannon'), 'Little Bird', 'spotify:track:3KntH6hRCc4HY6E70xMn8F'),
-    structures.Track(structures.Artist('Kate Rusby'), 'I\'ll Be Wise', 'spotify:track:5XMWL43ivHgti9I6LCqYPF'),
-    structures.Track(structures.Artist('The Wolfe Tones'), 'Skibereen', 'spotify:track:7sS4BK6MdXqu3fCqPXStPb'),
-    structures.Track(structures.Artist('The Dubliners'), 'Song For Ireland', 'spotify:track:26DVAZXVZ6vHgShGFW1Ebo'),
-    structures.Track(structures.Artist('Liam Clancy'), 'The Dutchman', 'spotify:track:2KKQwSx8WlYLFLMi6KAoEn'),
-]
-
-sample_artists = [
-    structures.Artist('Townes Van Zandt', 'spotify:artist:3ZWab2LEVkNKiBPIClTwof'),
-    structures.Artist('Christy Moore', 'spotify:artist:3Ebn7mKYzD0L3DaUB1gNJZ'),
-    structures.Artist('"George Jones', 'spotify:artist:2OpqcUtj10HHvGG6h9VYC5'),
-]
-
-sample_results.update({
-    'track': sample_tracks,
-    'artist': sample_artists,
-})
+        # This code won't last for long
+        module_dir, _ = os.path.split(__file__)
+        json_path = os.path.join(module_dir, 'searchresponses')
+        filename = '{0}-{1}.json'.format(search_type, query.lower().replace(' ', '_'))
+        try:
+            with open(os.path.join(json_path, filename), 'r', encoding='utf-8') as f:
+                data = json.load(f)
+        except (FileNotFoundError, ValueError):
+            return {} # No results
+        return data
 
