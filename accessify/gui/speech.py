e029215f323fdcd3b60332f3c28691c36f07ab1b
James Scholes <james@jamesscholes.com>
2017-04-09 20:08:07 +0100

    Add delay_ms parameter to gui.speech.speak

--- accessify/gui/speech.py
+++ accessify/gui/speech.py
@@ -3,6 +3,9 @@ import wx
 import tolk
 
 
-def speak(text):
-    wx.CallAfter(tolk.speak, text, False)
+def speak(text, delay_ms=0):
+    if delay_ms <= 0:
+        wx.CallAfter(tolk.speak, text, False)
+    else:
+        wx.CallLater(delay_ms, tolk.speak, text, False)
 
