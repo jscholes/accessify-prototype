jscholes/accessify-prototype

commit 28adaf56a2b48331644e0f96519d62af16f15c12

Only retry twice when incomplete metadata is received from Spotify, then show an 'Unknown Content' message in the title bar of the GUI

accessify/gui/main.py
@@ -25,6 +25,7 @@ MSG_LOADING = 'Loading...'
 MSG_NO_CONNECTION = '{0} cannot function without the Spotify client.  The application will now exit.'.format(constants.APP_NAME)
 MSG_NO_AUTHORISATION = 'You can\'t use {0} without a Spotify account.  The application will now exit.'.format(constants.APP_NAME)
 MSG_SPOTIFY_NOT_RUNNING = '{0} uses the Spotify client to play content, but it doesn\'t seem to be running.  Please start it up, log into your account and then restart {0}.'.format(constants.APP_NAME)
+MSG_UNKNOWN_CONTENT = 'Unknown Content'
 ERROR_UNPLAYABLE_CONTENT = 'The URI {uri} couldn\'t be played.  The content might not be available in your country or an advert might currently be playing.'
 
 ID_PLAY_PAUSE = wx.NewId()
@@ -89,8 +90,10 @@ class MainWindow(wx.Frame):
             if not self.IsShown():
                 self.Show()
 
-    def UpdateTrackDisplay(self, track):
-        if track is None:
+    def UpdateTrackDisplay(self, track=None, unknown_content=False):
+        if unknown_content:
+            self.SetTitle('{0} - {1}'.format(WINDOW_TITLE, MSG_UNKNOWN_CONTENT))
+        elif track is None:
             self.SetTitle(WINDOW_TITLE)
         else:
             self.SetTitle('{0} - {1}'.format(WINDOW_TITLE, format_track_display(track)))
@@ -110,7 +113,8 @@ class MainWindow(wx.Frame):
 
     @main_thread
     def onTrackChange(self, track):
-        self.UpdateTrackDisplay(track)
+        if track is None:
+            self.UpdateTrackDisplay(unknown_content=True)
 
     @main_thread
     def onPlaybackStateChange(self, state, track):

accessify/playback.py
@@ -74,6 +74,8 @@ class PlaybackController(pykka.ThreadingActor):
     def on_error(self, exception):
         if isinstance(exception, exceptions.ContentPlaybackError):
             self._signalman.unplayable_content.send(exception.uri)
+        elif isinstance(exception, exceptions.MetadataNotReadyError):
+            self._signalman.track_changed.send(None)
         else:
             self._connected = False
             self._signalman.error.send(exception)

accessify/spotify/eventmanager.py
@@ -33,6 +33,7 @@ class EventManager(threading.Thread):
     def run(self):
         consume_queue(self._event_queue, self._process_item)
         return_immediately = True
+        metadata_retries = 0
         while True:
             try:
                 if return_immediately:
@@ -41,8 +42,14 @@ class EventManager(threading.Thread):
                     status = self._remote_bridge.get_status(return_after=60)
                 self._event_queue.put(status)
                 return_immediately = False
-            except exceptions.MetadataNotReadyError:
-                return_immediately = True
+            except exceptions.MetadataNotReadyError as e:
+                if metadata_retries >= 3:
+                    metadata_retries = 0
+                    return_immediately = False
+                    self._event_queue.put(e)
+                else:
+                    return_immediately = True
+                    metadata_retries += 1
                 continue
             except exceptions.SpotifyError as e:
                 self._event_queue.put(e)
@@ -84,7 +91,7 @@ class EventManager(threading.Thread):
             self._playback_state = playback_state
 
     def _update_subscribers(self, event_type, context=None):
-        logger.debug('Updating subscribers to {0} with context: {1}'.format(event_type, context))
+        logger.debug('Updating subscribers to {0} with context: {1}'.format(event_type, repr(context)))
         for callback in self._callbacks[event_type]:
             self._update_subscriber(event_type, callback, context)
 

accessify/utils/concurrency.py
@@ -9,11 +9,11 @@ def consume_queue(a_queue, item_handler):
     """
     Fetch items from a queue on a background thread and dispatch them to the specified item_handler callable.
     """
-    logger.debug('Consuming queue {0} - dispatching items to {1}'.format(a_queue, item_handler))
     def consume():
         while True:
             try:
-                item_handler(a_queue.get())
+                item = a_queue.get()
+                item_handler(item)
                 a_queue.task_done()
             except Exception:
                 logger.exception('Item handler {0} threw an error:'.format(item_handler), exc_info=True)

commit 4edfd8e07d9a722da722a9198562e34200b16581

Fix a track change-related regression in the GUI introduced by commit 28adaf5

accessify/gui/main.py
@@ -113,8 +113,8 @@ class MainWindow(wx.Frame):
 
     @main_thread
     def onTrackChange(self, track):
-        if track is None:
-            self.UpdateTrackDisplay(unknown_content=True)
+        is_unknown = track is None
+        self.UpdateTrackDisplay(track, unknown_content=is_unknown)
 
     @main_thread
     def onPlaybackStateChange(self, state, track):